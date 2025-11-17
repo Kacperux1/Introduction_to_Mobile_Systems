7d4ed1796f4c8e660db0773c0bba9808e7b0ef4d
Kacperux1 <kacper34505@o2.pl>
2025-11-17 23:12:10 +0100

    init project structure

---
+++ rest/src/test/java/finlandia40/TestRestApplication.java
@@ -0,0 +1,11 @@
+package finlandia40;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(RestApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
